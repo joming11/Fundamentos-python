10cad3b75b398f1e2d07a8631f8a37cae3949aa1
Jose Navas <njose0791@gmail.com>
2024-05-07 21:57:37 -0400

    tipado dinamico en python

---
+++ fundamentos/tipado.py
@@ -0,0 +1,12 @@
+# tipado dinamico - capacidad que tienen las variables de almacenar distintos tipos de datos en distintos momentos
+valor = "Eduardo"
+print(type(valor))
+
+valor = 2
+print(type(valor))
+
+valor = 3.1
+print(type(valor))
+
+valor = True
+print(type(valor))
