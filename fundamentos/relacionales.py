141b95357bcc3e9ad98743c407537bfe3992f76d
Jose Navas <njose0791@gmail.com>
2024-05-07 22:13:07 -0400

    operadores relacionales en python

---
+++ fundamentos/relacionales.py
@@ -0,0 +1,17 @@
+# operadores relacionales
+#para evaluar numeros enteros o flotantes
+# al evaluarlos dan como resultado un booleano
+numero_uno = 20
+numero_dos = 50
+
+"""
+    >
+    <
+    >=
+    <=
+    == igual
+    != diferente
+"""
+
+resulto = numero_uno == numero_dos
+print(resulto)
