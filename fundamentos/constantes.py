dcca940221a7109ed70088f0f6ada29b9a642376
Jose Navas <njose0791@gmail.com>
2024-05-07 21:13:05 -0400

    convencion para definir constantes

---
+++ fundamentos/constantes.py
@@ -0,0 +1,5 @@
+# No existen las constantes en python
+# por convencion se define una variable en mayusculas para indicar que no se modificara su valor (aunque al ser una variable facilmente puede cambiar de valor)
+TITULO_CURSO = 'Curso profesional de Python'
+
+print(TITULO_CURSO)
