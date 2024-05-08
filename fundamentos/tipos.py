fbf203efc99e408da73aed669b12103fe23eb94b
Jose Navas <njose0791@gmail.com>
2024-05-07 21:50:10 -0400

    tipos de datos en python

---
+++ fundamentos/tipos.py
@@ -0,0 +1,29 @@
+# String
+titulo_curso = 'Curso profesional de Python'
+print(titulo_curso)
+
+nombre_completo = "Miguel Navas"
+print(nombre_completo)
+
+# se utilizan triple comillas simples o dobles para definir un texto que tiene saltos de linea, como el ejemplo a cotinuacion
+mensaje = '''Te encuentras
+en el curso: Profesional de Python.
+En CódigoFacilito'''
+
+print(mensaje)
+
+# Int
+# se pueden realizar las operaciones aritmeticas basicas, +, -, * y /
+# Al divir con / Obtenemos un tipo de dato flotante
+# Al divir con // Obtenemos un tipo de dato entero
+numero_uno = 10 // 3 
+print(numero_uno)
+
+# Float
+numero_dos = -3.14
+print(numero_dos)
+
+# Bool
+# True - False
+valor = False 
+print(valor)
