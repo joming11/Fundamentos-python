9b470228a99f45ae6583f3bca9aad45eb3febf8f
Jose Navas <njose0791@gmail.com>
2024-05-08 10:42:44 -0400

    funcion type

--- fundamentos/tipado.py
+++ fundamentos/tipado.py
@@ -1,5 +1,9 @@
 # tipado dinamico - capacidad que tienen las variables de almacenar distintos tipos de datos en distintos momentos
+
+# para saber que tipo de dato tiene una variable
 valor = "Eduardo"
+#tipo = type(valor)
+#print(tipo)
 print(type(valor))
 
 valor = 2
