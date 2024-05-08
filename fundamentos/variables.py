94c5cf06a071be77399cf7c8cb24bd2e0ce56e12
Jose Navas <njose0791@gmail.com>
2024-05-07 21:02:36 -0400

    variables en python

---
+++ fundamentos/variables.py
@@ -0,0 +1,17 @@
+# en python las variables no se definen como una espacio para almacenar un valor, sino como una etiqueta
+
+# definir una variable
+# utilizar minusculas
+# si el nombre de la variable tiene mas de un nombre unir con guion bajo
+# nombre = valor
+titulo_curso = 'Curso profesional de Python'
+
+# print(titulo_curso)
+
+# las variables se pueden redeclarar y cambiar el valor
+
+# titulo_curso = 'Curso python'
+
+nombre_completo = 'José Miguel Navas Gutierrez'
+
+print(titulo_curso)
