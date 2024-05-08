cb427a13eb9b7d6f9f3ea10d511d9919db1cede6
Jose Navas <njose0791@gmail.com>
2024-05-08 10:35:22 -0400

    operadores logicos en python

---
+++ fundamentos/logicos.py
@@ -0,0 +1,21 @@
+# and, or y not
+
+# and evalua que dos o mas condiciones sean true
+# en python no se usa &&, solo se escribe and
+# se pueden usar el operador and las veces que sean necesarias
+
+# resultado_final = True and True
+# resultado_final = True and True and True and True
+# tambien se puede combinar con operadores aritmeticos 
+# resultado_final = True and True and True and 5 > 2
+
+#or evalua que una de dos o mas comparaciones se true
+# no es necesario usar ||, solo escribir or
+
+#se pueden combinar operadores logicos
+# resultado_final = True and ( False or 5 > 2)
+# el resultado sera True
+
+#not negar, cambiara el valor
+resultado_final = not True
+print(resultado_final)
