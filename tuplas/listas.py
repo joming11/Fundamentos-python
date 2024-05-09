fdfd18e8712caf9f615246ba7f13e8017276f085
Jose Navas <njose0791@gmail.com>
2024-05-08 21:59:33 -0400

    generar tuplas a partir de listas y viceversa

---
+++ tuplas/listas.py
@@ -0,0 +1,15 @@
+# generar tuplas a partir de listas
+cursos = ['Python', 'Django', 'Flask']
+
+cursos_tupla = tuple(cursos)
+
+print(cursos_tupla)
+print(type(cursos_tupla))
+
+# generar listas a partir de tuplas
+niveles = ('Básico', 'Intermedio', 'Avanzado')
+
+niveles_lista = list(niveles)
+
+print(niveles_lista)
+print(type(niveles_lista))
