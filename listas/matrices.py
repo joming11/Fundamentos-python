55ecd13589215bb2afef2301ddc978faafd2f216
Jose Navas <njose0791@gmail.com>
2024-05-08 21:22:57 -0400

    matrices en python

---
+++ listas/matrices.py
@@ -0,0 +1,11 @@
+# matrices en python
+# lista_a = [15, 11]
+# lista_b = [12, 10]
+# matriz = [lista_a, lista_b] matriz 2x2
+
+columna_a = [10, 20, 30, 40]
+columna_b = [30, 40, 50, 60]
+
+matriz = [columna_a, columna_b] # 2 x 4
+
+print(matriz[1][3])
