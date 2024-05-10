4668490a22b209f196c6de274653309ab5276cab
Jose Navas <njose0791@gmail.com>
2024-05-10 12:45:25 -0400

    ciclo for en python

---
+++ condiciones/for.py
@@ -0,0 +1,9 @@
+# ciclo for en python
+# es como un foreach
+# para iterar dentro de una coleccion de valores
+
+# la variable usuario a medida que vaya iterando tomara el valor de cada indice, en la primera iteracion, su valor sera usuario1
+usuarios = ['usuario1', 'usuario2', 'usuario3', 'usuario4']
+
+for usuario in usuarios:
+    print(usuario)
