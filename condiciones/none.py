b01aed524068139668005d2d048c371b3adee5f4
Jose Navas <njose0791@gmail.com>
2024-05-10 10:54:09 -0400

    uso de None en python

---
+++ condiciones/none.py
@@ -0,0 +1,22 @@
+# tipo de dato vacio none
+# se usa para representar la ausencia de valor
+resultado = None # no almacena ningun valor, si se evalua el tipo, sera None y a nivel booleano, sera false
+
+resultado = [1, 2, 3]
+
+print(resultado)
+print(type(resultado))
+
+# True (1) / False (0)
+
+#valore que arrojan False
+"""
+False
+None
+0
+0.0
+'' / ""
+[]
+()
+{}
+"""
