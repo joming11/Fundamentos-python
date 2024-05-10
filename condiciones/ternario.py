84c2e0357adfe33963f719e46764b3a2c8aa077c
Jose Navas <njose0791@gmail.com>
2024-05-10 11:52:22 -0400

    operador ternario en python

---
+++ condiciones/ternario.py
@@ -0,0 +1,15 @@
+# operador ternario
+
+# calificacion = 10
+# color = None
+
+#if calificacion >= 7:
+    #color = 'verde?
+#else:
+    #color = 'rojo'
+#print(calificacion, color)    
+calificacion = 10
+
+color = 'Verde' if calificacion >= 7 else 'Rojo'
+
+print(calificacion, color)
