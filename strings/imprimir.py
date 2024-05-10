522bd0b07390de43ec3030e0995c205888fa91e4
Jose Navas <njose0791@gmail.com>
2024-05-09 21:05:37 -0400

    impresion en consola

---
+++ strings/imprimir.py
@@ -0,0 +1,8 @@
+#imprimir en python
+nombre = 'Raquel'
+apellido = 'Manzanilla'
+
+#sep=' ' permite definir como imprimir en consola
+# por default es con espacio
+# si queremos separar con otro caracter se usa sep='-' o sep='/'
+print(nombre, apellido, 'Peña', sep=' ')
