e6da95550b5ef7d07ff8cf41a623c3e090095c95
Jose Navas <njose0791@gmail.com>
2024-05-10 11:12:59 -0400

    condiciones en python

---
+++ condiciones/condiciones.py
@@ -0,0 +1,21 @@
+# condicionales
+# valor inicial
+resultado = 15
+
+# se usara if para evaluar si una condicion es verdadera o falsa
+# if <bool>:
+# por convencion, en python, se identa con 4 espacios para colocar la expresion de la evaluacion da la condicion
+
+#ejemplo resultado = 10
+# resultado = resultado > 10
+
+#if resultado:
+    #print(´´se cumple la condicion)
+
+#e ejecutara el print si la condicion es true
+
+
+if resultado > 10 and resultado < 20:
+    print('La variable cumple con la condición.')
+else:
+    print('La condición no se cumplio :( ')
