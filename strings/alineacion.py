723d55933a1bef03f75583f7cf7a019fa47a3b75
Jose Navas <njose0791@gmail.com>
2024-05-09 21:45:18 -0400

    justificacion de texto

---
+++ strings/alineacion.py
@@ -0,0 +1,13 @@
+#alinear un texto
+mensaje = 'Hola mundo!'
+
+# ljust -> Justificar a la Izquierda
+# rjust -> Justificar a la Derecha
+# center -> Centrar
+
+# mensaje = mensaje.ljust(cantidad de caracteres que movera a la izquiera)
+# mensaje = mensaje.rjust(cantidad de caracteres que movera a la derecha)
+# mensaje = mensaje.center(cantidad de caracteres que movera a la derecha y a la izquierda)
+mensaje = mensaje.center(20)
+
+print(mensaje)
